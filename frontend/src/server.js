afe728cd1e7829e50ba1481908166bee98854d82
Dmih_Dang9 <minhdang09032002@gmail.com>
2023-07-06 01:28:36 +0700

    feat:

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://e-commerse-website-9whey88r2-doanminhdang93.vercel.app/api/v2";
-export const backend_url = "https://e-commerse-website-9whey88r2-doanminhdang93.vercel.app/";
+export const server = "https://e-commerse-website-hnzgwv9bs-doanminhdang93.vercel.app/api/v2";
+export const backend_url = "https://e-commerse-website-hnzgwv9bs-doanminhdang93.vercel.app/";
