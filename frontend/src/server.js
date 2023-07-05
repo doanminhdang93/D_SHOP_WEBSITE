77b029d65fd275784bd250aaac5c39b579b1d1b6
Dmih_Dang9 <minhdang09032002@gmail.com>
2023-07-06 02:42:01 +0700

    feat:

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://d-shop-website.vercel.app/api/v2";
-export const backend_url = "https://d-shop-website.vercel.app/";
+export const server = "http://localhost:8000/api/v2";
+export const backend_url = "http://localhost:8000/";
