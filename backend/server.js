9dd0dac869d183011ac7be8f34cb5746ac375b56
Dmih_Dang9 <minhdang09032002@gmail.com>
2023-07-06 23:06:49 +0700

    feat:

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const connectDatabase = require("./db/Database");
+const connectDatabase = require("./db/database");
 const cloudinary = require("cloudinary");
 
 // Handling uncaught Exception
